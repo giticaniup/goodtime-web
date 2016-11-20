c3cbb73838ef797711138c63d54c2b75d494e7a8
钟乘永 <zhongcy93@163.com>
2016-11-20 13:07:21 +0800

    代码提交

--- gd-user/src/main/java/com/goodtime/user/controller/TaskController.java
+++ gd-user/src/main/java/com/goodtime/user/controller/TaskController.java
@@ -9,6 +9,7 @@ import com.goodtime.user.utils.UserConstants;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
