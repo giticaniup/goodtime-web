82568e199e3ec0748a7604b9f3223248993af1ce
钟乘永 <zhongcy93@163.com>
2016-12-01 11:59:52 +0800

    代码提交

---
+++ gd-user/src/main/java/com/goodtime/user/controller/DiaryGroupController.java
@@ -0,0 +1,52 @@
+package com.goodtime.user.controller;
+
+import com.github.api.entity.DiaryGroup;
+import com.github.api.service.DiaryGroupService;
+import com.goodtime.base.result.BaseResult;
+import com.goodtime.base.result.Result;
+import com.goodtime.user.utils.UserConstants;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 日志分类Controller
+ * Created by zhongcy on 2016/11/30.
+ */
+@RestController
+public class DiaryGroupController extends BaseController{
+
+    @Autowired
+    private DiaryGroupService diaryGroupService;
+
+    @RequestMapping(value = "/goodtime/diaryGroup",method = RequestMethod.GET)
+    @RequiresAuthentication
+    public BaseResult getDiaryGroup(HttpSession session){
+        Integer userId = (Integer) session.getAttribute(UserConstants.CURRENT_USER);
+        return new BaseResult<>(diaryGroupService.getDiaryGroup(userId));
+    }
+
+    @RequestMapping(value = "/goodtime/diaryGroup",method = RequestMethod.GET)
+    @RequiresAuthentication
+    public Result saveDiaryGroup(HttpSession session, @RequestBody DiaryGroup diaryGroup){
+        Integer userId = (Integer) session.getAttribute(UserConstants.CURRENT_USER);
+        diaryGroup.setCreator(userId);
+        diaryGroupService.addDiaryGroup(diaryGroup);
+        return SUCCESS;
+    }
+
+    public Result updateDiaryGroup(@RequestBody DiaryGroup diaryGroup){
+        diaryGroupService.updateDiaryGroup(diaryGroup);
+        return SUCCESS;
+    }
+
+    public Result deleteDiaryGroup(Integer id){
+        diaryGroupService.deleteDiaryGroup(id);
+        return SUCCESS;
+    }
+}
